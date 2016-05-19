e33ff7cdbc19ff040878b88f06de1188ffcdf0a4
Ants Kristjan Rooma <antskristjan@hotmail.com>
2016-05-19 13:44:19 +0300

    Initial push with base project for client application.

---
+++ ClientApp.Arvutirent/ComputerRentalClientApp/Services/BaseService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerRentalClientApp.Services
+{
+    public class BaseService
+    {
+        private HttpClient _client;
+
+        public BaseService(string serviceUrl)
+        {
+            this._client = new HttpClient();
+            _client.BaseAddress = new Uri(serviceUrl);
+        }
+
+        public async Task<T> getData<T>(string url)
+        {
+            HttpResponseMessage response = await _client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsAsync<T>();
+        }
+
+        public async Task<T> postData<T>(string url, T obj)
+        {
+            var response = await _client.PostAsJsonAsync<T>(url, obj);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsAsync<T>();
+        }
+
+        public async void updateData(string url, object obj)
+        {
+            var response = await _client.PutAsJsonAsync(url, obj);
+            response.EnsureSuccessStatusCode();
+
+        }
+
+        public async void deleteData(string url)
+        {
+            var response = await _client.DeleteAsync(url);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
---
+++ ClientApp.Arvutirent/ComputerRentalClientApp/Services/ServiceConstants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ComputerRentalClientApp.Services
+{
+    public static class ServiceConstants
+    {
+        private static string getValue(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+
+        public static string ComputerServiceUrl
+        {
+
+            get { return getValue("ComputerServiceUrl"); }
+        }
+    }
+}
